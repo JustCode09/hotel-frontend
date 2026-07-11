84e24c4c5a2eec2a73a652ca6654306be89a3ef3
JustCode09 <Professionalacc011@gmail.com>
2026-07-11 10:07:40 +0545

    Created Navbar, Hero, Footer components

---
+++ src/Hero.js
@@ -0,0 +1,11 @@
+function Hero() {
+  return (
+    <div>
+      <h1>Welcome to Hotel Booking 🏨</h1>
+      <p>Find and book your perfect room</p>
+      <button>Browse Rooms</button>
+    </div>
+  );
+}
+
+export default Hero;
---
+++ src/Navbar.js
@@ -0,0 +1,15 @@
+function Navbar() {
+  return (
+    <nav>
+      <h2>🏨 Hotel Booking</h2>
+      <ul>
+        <li>Home</li>
+        <li>Rooms</li>
+        <li>About</li>
+        <li>Contact</li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Navbar;
