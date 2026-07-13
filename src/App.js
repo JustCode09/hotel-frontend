445b478745727de8948eea05bad1cfc803ecf1ea
JustCode09 <Professionalacc011@gmail.com>
2026-07-13 08:16:21 +0545

    Day 4 - useState, like button on room cards

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
+import { useState } from 'react';
 import Navbar from './Navbar';
-import Hero from './Hero';
 import Footer from './Footer';
 import RoomCard from './RoomCard';
 
@@ -7,15 +7,11 @@ function App() {
   return (
     <div>
       <Navbar />
-      <Hero />
-       
-      <h1>Welcome to Hotel Booking!</h1>
       <h2>Available Rooms</h2>
       <RoomCard name="Deluxe Room" price="5000" view="Mountain View" />
       <RoomCard name="Standard Room" price="3000" view="City View" />
       <RoomCard name="Suite Room" price="8000" view="Pool View" />
-    
-    <Footer />
+      <Footer />
     </div>
   );
 }
